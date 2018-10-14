65582822ce84f72bbd797ac6b23b87507d5791c3
clems <gervaise.clement@gmail.com>
2018-10-14 19:11:42 +0200

    init

---
+++ src/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+
+export default () => (
+    <Card>
+        <CardHeader title="Salut !" />
+        <CardContent>
+             erogoiehzoiehro _h"'_çh fuihihdfeusdbjk j uidbi  uihjl
+        </CardContent>
+    </Card>
+);
